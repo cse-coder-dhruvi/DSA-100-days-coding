efd590b7c1fc25ca824381b16f30cd7ba2587fda
cse-coder-dhruvi <dhruvi.21818@stu.upes.ac.in>
2026-02-26 21:17:21 +0530

    Update Q1.c

--- day23/Q1.c
+++ day23/Q1.c
@@ -1,93 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+Problem: Merge Two Sorted Linked Lists - Implement using linked list with dynamic memory allocation.
 
-// Node structure
-struct Node {
-    int data;
-    struct Node* next;
-};
+Input:
+- First line: integer n
+- Second line: n space-separated integers (first list)
+- Third line: integer m
+- Fourth line: m space-separated integers (second list)
 
-// Function to create a new node
-struct Node* createNode(int value) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = value;
-    newNode->next = NULL;
-    return newNode;
-}
+Output:
+- Print the merged linked list elements, space-separated
 
-// Insert at end
-struct Node* insertEnd(struct Node* head, int value) {
-    struct Node* newNode = createNode(value);
+Example:
+Input:
+5
+10 20 30 40 50
+4
+15 25 35 45
 
-    if (head == NULL)
-        return newNode;
+Output:
+10 15 20 25 30 35 40 45 50
 
-    struct Node* temp = head;
-    while (temp->next != NULL)
-        temp = temp->next;
-
-    temp->next = newNode;
-    return head;
-}
-
-// Merge two sorted lists
-struct Node* mergeLists(struct Node* l1, struct Node* l2) {
-    struct Node dummy;   // temporary dummy node
-    struct Node* tail = &dummy;
-    dummy.next = NULL;
-
-    while (l1 != NULL && l2 != NULL) {
-        if (l1->data < l2->data) {
-            tail->next = l1;
-            l1 = l1->next;
-        } else {
-            tail->next = l2;
-            l2 = l2->next;
-        }
-        tail = tail->next;
-    }
-
-    // Attach remaining nodes
-    if (l1 != NULL)
-        tail->next = l1;
-    else
-        tail->next = l2;
-
-    return dummy.next;
-}
-
-// Print list
-void printList(struct Node* head) {
-    while (head != NULL) {
-        printf("%d ", head->data);
-        head = head->next;
-    }
-}
-
-int main() {
-    int n, m, x;
-    struct Node* list1 = NULL;
-    struct Node* list2 = NULL;
-
-    // Input first list
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &x);
-        list1 = insertEnd(list1, x);
-    }
-
-    // Input second list
-    scanf("%d", &m);
-    for (int i = 0; i < m; i++) {
-        scanf("%d", &x);
-        list2 = insertEnd(list2, x);
-    }
-
-    // Merge
-    struct Node* merged = mergeLists(list1, list2);
-
-    // Output
-    printList(merged);
-
-    return 0;
-}
+Explanation:
+Compare nodes of both lists, append smaller to result, continue until all nodes are merged.
