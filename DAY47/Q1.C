ed078240e7b44268ea0c48391d9f5156b7793fac
cse-coder-dhruvi <dhruvi.21818@stu.upes.ac.in>
2026-03-21 21:15:25 +0530

    Create Q1.C

---
+++ DAY47/Q1.C
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure for tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* newNode(int value) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = value;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+// Function to find height of tree
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
+}
+
+// Main function
+int main() {
+    // Creating sample tree
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("Height of Binary Tree: %d\n", height(root));
+
+    return 0;
+}
