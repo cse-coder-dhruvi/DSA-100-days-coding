73bb94d5edc53dc22b88103f98d869a9da0a2284
cse-coder-dhruvi <dhruvi.21818@stu.upes.ac.in>
2026-03-28 08:07:22 +0530

    Create Q1.c

---
+++ day57/Q1.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Create new node
+struct Node* newNode(int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// Check mirror
+int isMirror(struct Node* left, struct Node* right) {
+    if (left == NULL && right == NULL)
+        return 1;
+
+    if (left == NULL || right == NULL)
+        return 0;
+
+    if (left->data != right->data)
+        return 0;
+
+    return isMirror(left->left, right->right) &&
+           isMirror(left->right, right->left);
+}
+
+// Build tree using level order
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct Node* root = newNode(arr[0]);
+    struct Node* queue[n];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int i = 1;
+
+    while (i < n) {
+        struct Node* curr = queue[front++];
+
+        // Left child
+        if (arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        // Right child
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    if (isMirror(root, root))
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
