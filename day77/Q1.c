202f638182346cfd2678a6d8f101bca8b4074d06
cse-coder-dhruvi <dhruvi.21818@stu.upes.ac.in>
2026-04-18 22:18:17 +0530

    Add DFS implementation for graph connectivity check
    Implement Depth First Search (DFS) to check graph connectivity.

---
+++ day77/Q1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+int visited[MAX];
+int adj[MAX][MAX];
+int n;
+
+void dfs(int v) {
+    visited[v] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (adj[v][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int m;
+    scanf("%d %d", &n, &m);
+
+    // Initialize adjacency matrix
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+        visited[i] = 0;
+    }
+
+    // Read edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1; // undirected graph
+    }
+
+    // Run DFS from node 1
+    dfs(1);
+
+    // Check if all nodes are visited
+    int connected = 1;
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            connected = 0;
+            break;
+        }
+    }
+
+    if (connected)
+        printf("CONNECTED\n");
+    else
+        printf("NOT CONNECTED\n");
+
+    return 0;
+}
